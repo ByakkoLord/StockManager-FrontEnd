5455bf8d55ed23d26d2a4011030488f6f660b4c0
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-12 22:49:34 -0300

    Add Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import Login from './pages/Login.tsx'
 import './index.css'
+
+import CreateAccount from './pages/CreateAccount.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+        
+        <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/createAccount" element={<CreateAccount/>} />
+        </Routes>
+      </Router>
   </React.StrictMode>,
 )
