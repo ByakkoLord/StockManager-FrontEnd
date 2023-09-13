27a146ff34674b4c5637228c331e529dbb49f558
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-13 20:34:00 -0300

    add error message component

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,11 +1,16 @@
+import { IconContext } from 'react-icons/'
+import { BiErrorCircle } from 'react-icons/bi'
+
 export default function ErrorMessage() {
 
-    
+
 
     return (
         <div className="absolute bg-red-600 p-2 rounded-md shadow-xl left-10 bottom-10 flex justify-center items-center">
-            <h1 className="roboto text-xl pr-4 font-bold text-white">Error:</h1>
-            <p className="roboto text-xl font-bold text-white">Please fill in all fields</p>
+            <IconContext.Provider value={{ size: "2rem", className: "text-white mr-2" }}>
+                <BiErrorCircle />
+            </IconContext.Provider>
+            <p className="poppins text-xl mr-2 font-bold text-white">Please fill in all fields</p>
         </div>
     )
 }
