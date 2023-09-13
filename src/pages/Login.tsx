74824fad306ead5856532e9086a6a6ccd21d79f4
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-12 22:49:21 -0300

    Add CreateAccount Link

---
+++ src/pages/Login.tsx
@@ -0,0 +1,36 @@
+import LoginBlock from './LoginBlock'
+
+//React Icons
+
+import { IconContext } from 'react-icons/'
+import { BsFillBoxSeamFill } from 'react-icons/bs'
+
+//Components
+
+
+
+
+
+function App() {
+
+  return (
+    <div className='flex bg-gray-900 w-screen h-screen'>
+      <div className='flex flex-col w-1/2 justify-center items-center'>
+      <IconContext.Provider 
+      value={{ 
+        color: "#ceed2e",
+        size: "15rem",
+        className: "global-class-name" }}>
+        <BsFillBoxSeamFill/>
+      </IconContext.Provider>
+      <h1 className='text-4xl mt-5 font-bold text-white'>Stock Manager</h1>
+      </div>
+      <section className='flex w-1/2 justify-center items-center'>
+        <LoginBlock/>
+      </section>
+      
+    </div>
+  )
+}
+
+export default App
