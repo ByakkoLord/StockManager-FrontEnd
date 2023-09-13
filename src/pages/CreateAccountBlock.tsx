6ca4fb8c204b15a6f832e7bc06968c5fcb4e1d88
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-12 22:58:07 -0300

    add confirm password input

--- src/pages/CreateAccountBlock.tsx
+++ src/pages/CreateAccountBlock.tsx
@@ -3,8 +3,10 @@ import React, { useState } from 'react';
 export default function Login() {
   const [inputValue, setInputValue] = useState('');
   const [inputValue2, setInputValue2] = useState('');
+  const [inputValue3, setInputValue3] = useState('');
   const [height, setHeight] = useState(false);
   const [height2, setHeight2] = useState(false);
+  const [height3, setHeight3] = useState(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
@@ -30,6 +32,21 @@ export default function Login() {
         console.log('Input contém texto:', value2);
       } else {
         setHeight2(false);
+        console.log('Input está vazio');
+      }
+  };
+
+  const handleInputChange3 = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+    const value3 = event.target.value;
+    setInputValue3(value3);
+
+
+    if (value3) {
+        setHeight3(true);
+        console.log('Input contém texto:', value3);
+      } else {
+        setHeight3(false);
         console.log('Input está vazio');
       }
   };
@@ -61,13 +78,24 @@ export default function Login() {
           name="password"
           id="password"
         />
+        <label className="hover:cursor-pointer hover:text-white mt-5 text-gray-300 font-semibold" htmlFor="confirmPassword">
+          Confirm Password
+        </label>
+        <input
+          value={inputValue3}
+          onChange={handleInputChange3}
+          className={`transition-all pb-1 text-gray-300 font-semibold focus:outline-none outline-none bg-transparent border-b-2 border-gray-400 ${height3 ? 'h-8' : 'h-0'}`}
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+        />
       </section>
       
 
       <button
         className={`transition-all roboto hover:bg-slate-500 text-base rounded-xl primaryColor p-1 pr-16 pl-16 mt-5 font-bold text-black`}
       >
-        Login
+        Create
       </button>
     </form>
 
