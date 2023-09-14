3394e236f83870bbae79c05f0d9a1c6d4c9c1ef6
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-13 21:30:40 -0300

    add animation, show and errorMessage props

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,16 +1,27 @@
+import style from './ErrorMessage.module.css'
+
 import { IconContext } from 'react-icons/'
 import { BiErrorCircle } from 'react-icons/bi'
 
-export default function ErrorMessage() {
 
 
+interface Props {
+    errorMessage: string,
+    show: boolean,
+    animation: string
+}
+
+export default function ErrorMessage(prop: Props) {
+const { errorMessage, show, animation } = prop
+    
 
     return (
-        <div className="absolute bg-red-600 p-2 rounded-md shadow-xl left-10 bottom-10 flex justify-center items-center">
+        <div className={`${show ? 'flex' : 'hidden'} ${style[animation]}  absolute bg-red-600 p-2 rounded-md shadow-xl left-10 bottom-10 justify-center items-center`}>
             <IconContext.Provider value={{ size: "2rem", className: "text-white mr-2" }}>
                 <BiErrorCircle />
             </IconContext.Provider>
-            <p className="poppins text-xl mr-2 font-bold text-white">Please fill in all fields</p>
+            <p className="poppins text-xl mr-2 font-bold text-white">{errorMessage}</p>
         </div>
     )
 }
+
