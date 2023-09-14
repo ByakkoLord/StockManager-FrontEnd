6326f029981b8559b4a8fafecd60707b0c87e80c
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-14 17:37:40 -0300

    add errorMessage component

--- src/pages/LoginBlock.tsx
+++ src/pages/LoginBlock.tsx
@@ -1,65 +1,100 @@
 //Components
 
-import React, { useState } from 'react';
-import {Link} from 'react-router-dom'
+import React, { useState, FormEvent } from 'react';
+import {Link, useNavigate} from 'react-router-dom'
 
 //Api
 
+import api from '../services/api';
 
+import ErrorMessage from '../components/ErrorMessage';
 
 
 export default function Login() {
 
-// Api Functions
 
-  
+
+
 
 // Page Functions
 
-  const [inputValue, setInputValue] = useState('');
-  const [inputValue2, setInputValue2] = useState('');
+  const [errorString, setErrorString] = useState('');
+  const [show, setShow] = useState(false);
+  const [animation, setAnimation] = useState('');
+
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [height, setHeight] = useState(false);
   const [height2, setHeight2] = useState(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
-    setInputValue(value);
+    setEmail(value);
 
     if (value) {
       setHeight(true);
-      console.log('Input contém texto:', value);
     } else {
       setHeight(false);
-      console.log('Input está vazio');
     }
 
   }
   const handleInputChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
 
     const value2 = event.target.value;
-    setInputValue2(value2);
+    setPassword(value2);
 
 
     if (value2) {
         setHeight2(true);
-        console.log('Input contém texto:', value2);
       } else {
         setHeight2(false);
-        console.log('Input está vazio');
       }
   };
 
-  return (
+  // Api Functions
 
+  const navigate = useNavigate();
+
+  async function handleSubmitLogin(e: FormEvent) {
+    e.preventDefault();
+    
+        try {
+          const response = await api.post('/login', {
+            email,
+            password,
+          })
+          
+          if (response === undefined || response === null || response.data === 'Email or Password are incorrect') {
+            setErrorString('Email or Password are incorrect')
+            setAnimation('enterAnimation')
+            setShow(true)
+            setTimeout(() => {
+            setAnimation('exitAnimation')
+            setTimeout(() => {
+            setShow(false)
+        },1000)
+      },2500)
+          }else if (response.data === 'Email and Password are correct') {
+            navigate('/home')
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    
+
+  return (
+    <>
     <form className="flex flex-col justify-center p-5 items-center rounded-2xl shadow-2xl bg-gray-800">
-      <h1 className="text-3xl pr-5 mt-5 font-bold text-white"> &gt;Login</h1>
+      <h1 className="poppins text-3xl pr-5 mt-5 font-bold text-white"> &gt;Login</h1>
 
       <section className="flex flex-col mb-5">
         <label className="hover:cursor-pointer hover:text-white mt-5 text-gray-300 font-semibold" htmlFor="email">
           E-mail
         </label>
         <input
-          value={inputValue}
+          value={email}
           onChange={handleInputChange}
           className={`transition-all pb-1 text-gray-300 font-semibold focus:outline-none outline-none bg-transparent border-b-2 border-gray-400 ${height ? 'h-8' : 'h-0'}`}
           type="text"
@@ -71,7 +106,7 @@ export default function Login() {
           Password
         </label>
         <input
-          value={inputValue2}
+          value={password}
           onChange={handleInputChange2}
           className={`transition-all pb-1 text-gray-300 font-semibold focus:outline-none outline-none bg-transparent border-b-2 border-gray-400 ${height2 ? 'h-8' : 'h-0'}`}
           type="password"
@@ -90,10 +125,14 @@ export default function Login() {
       
 
       <button
+        onClick={handleSubmitLogin}
         className={`transition-all roboto hover:bg-slate-500 text-base rounded-xl primaryColor p-1 pr-16 pl-16 mt-5 font-bold text-black`}>
         Login
       </button>
     </form>
+    
+    <ErrorMessage animation={animation} show={show} errorMessage={errorString}/>
+    </>
 
     
   );
