d1f358781162504fe6af7ddd1a5b3bbaea82d778
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-14 22:22:08 -0300

    add useState Hook

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,21 @@
 import Header from '../../components/Header'
 import Aside from '../../components/Aside'
+import CreateStockpile from '../../components/CreateStockpile'
+
+import { useState } from 'react'
 
 
 
 export default function Home(){
+
+    const [displayCreateStockpile, setDisplayCreateStockpile] = useState(false)
+
+    const handleCreateStockpile = () => {
+        setDisplayCreateStockpile(true)
+    }
+
     return(
+        <>
         <div className="bg-gray-900 w-screen h-screen">
             <Header/>
             <section className='flex'>
@@ -15,15 +26,17 @@ export default function Home(){
                             <p className='poppins text-2xl text-slate-500'>Select some Stockpile</p>
                         </div>
                         <span className='poppins text-2xl text-slate-500 ml-5 mr-5'>Or</span>
-                        <button className='hover:border-slate-500 flex justify-center items-center w-80 h-44 border-2 border-gray-600 rounded-2xl'>
+                        <button onClick={handleCreateStockpile}
+                                className='hover:border-slate-500 flex justify-center items-center w-80 h-44 border-2 border-gray-600 rounded-2xl'>
                             <p className='poppins text-2xl text-slate-500'>Create a new one</p>
                         </button>
                     </section>
                     
                 </section>
             </section>
-            
-            
+
+            <CreateStockpile displayCreateStockpile={displayCreateStockpile}/> 
         </div>
+        </>
     )   
 }
