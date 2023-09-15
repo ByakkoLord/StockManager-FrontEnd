665e1dbad15362779f4f32d6cf1a7c5d9c8a5013
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-14 22:21:53 -0300

    add useState Hook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,17 @@
 import { IconContext } from 'react-icons/'
 import { BsFillBoxSeamFill } from 'react-icons/bs'
 import { AiOutlinePlus } from 'react-icons/ai'
+import CreateStockpile from '../components/CreateStockpile'
+
+import { useState } from 'react'
+
 
 export default function Header() {
+    const [displayCreateStockpile, setDisplayCreateStockpile] = useState(false)
+
     return (
-        <header className="flex fixed z-50 bg-gray-800 shadow-xl w-screen h-16">
+        <>
+        <header className="flex fixed z-40 bg-gray-800 shadow-xl w-screen h-16">
                 <div className='flex w-96 pl-5 justify-start items-center border-r-2 border-slate-500 mt-2 mb-2'>
                 <IconContext.Provider 
                     value={{ 
@@ -23,7 +30,7 @@ export default function Header() {
                               color: "#ceed2e",
                               size: "2rem",
                               className: "global-class-name" }}>
-                              <button className='transtion-all hover:scale-125'><AiOutlinePlus/></button>
+                              <button onClick={() => setDisplayCreateStockpile(true)} className='transtion-all hover:scale-125'><AiOutlinePlus/></button>
                         </IconContext.Provider>
                     
                 </div>
@@ -32,5 +39,8 @@ export default function Header() {
                     <div className='flex rounded-full w-12 h-12 bg-red-600 justify-center items-center'><span className='poppins text-2xl text-white'>BL</span></div>
                 </div>
             </header>
+            
+            <CreateStockpile displayCreateStockpile={displayCreateStockpile}/>
+        </>
     )
 }
