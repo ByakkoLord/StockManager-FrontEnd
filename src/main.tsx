da5d8ba7d5f39e8fee4f304f0a2b823c8f612aba
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-16 12:36:33 -0300

    erased test routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
-import Login from './pages/Login.tsx'
-import Teste from './pages/teste.tsx'
 import './index.css'
 
+import Login from './pages/Login.tsx'
 import CreateAccount from './pages/CreateAccount.tsx'
 import Home from './pages/home/Home.tsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <Route path="/" element={<Login/>} />
           <Route path="/createAccount" element={<CreateAccount/>} />
           <Route path='/home'element={<Home/>}/>
-          <Route path='/teste'element={<Teste/>}/>
+          
         </Routes>
       </Router>
   </React.StrictMode>,
