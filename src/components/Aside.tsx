075133c22e0dba9e3a83f80abe8acfff1fb92bee
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-16 00:26:05 -0300

    fixed newcomponent stockpile bug

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,15 +1,34 @@
 
 import { IconContext } from 'react-icons/'
 import {  AiFillSetting } from 'react-icons/ai'
-
+import { useState, useEffect } from 'react'
+import api from '../services/api'
 import StockpilesInfo from './StockpilesInfo'
 
 export default function Aside() {
+
+    const [allStockpiles, setAllStockpiles] = useState<{_id: string, stockpile: string, type: string, enterprise: string}[]>([])
+    
+
+    useEffect(() => {
+        async function getStockpiles(){
+            const response = await api.get('/allStockpiles')
+            setAllStockpiles(response.data)
+            console.log(response.data)
+        }
+        getStockpiles()
+    },[])
+
+
     return(
         <aside className='flex flex-col bg-gray-800 w-1/4 h-screen shadow-xl pt-16'>
                 <h2 className='text-center mt-4 poppins text-2xl text-white'>Stockpiles</h2>
                 <section className='flex flex-col mt-5 w-full h-full overflow-auto'>
-                    <StockpilesInfo/>
+
+                {allStockpiles.map(data => (
+                   <StockpilesInfo key={data._id} stockpileName={data.stockpile} typeName={data.type} enterpriseName={data.enterprise}/>
+                ))}
+                    
                     <button className='hover:scale-105 hover:border-gray-400 hover:text-slate-300 cursor-pointer rounded-sm transition-all flex h-14 ml-4 mr-4 justify-center items-center border-b-2 poppins text-xl text-slate-400  border-gray-600'>
                         Tables
                     </button>
