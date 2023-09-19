0f7727967acca70fd52cfe6608b839a46ba8e31a
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-19 20:54:55 -0300

    finished Token generator

--- src/contexts/Token.tsx
+++ src/contexts/Token.tsx
@@ -1,38 +1,50 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 import CryptoJS from "crypto-js";
 
 
-export const TokenContext = createContext<string | null>('')
+export const TokenContext = createContext<{
+    token: string
+    setToken: (token: string) => void;
+  setUsername: (username: string) => void;
+  generateToken: (data: string, secret: string) => string;
+  secretKey: string;
+}>({ token: "" , setToken: () => {} ,setUsername: () => {}, generateToken: () => "", secretKey: "" });
 
-export const TokenProvider = ({ children } : { children: ReactNode }) => {
+export const TokenProvider = ({ children }: { children: ReactNode }) => {
   // Chave secreta para assinar o token
-  const secretKey = 'byakko';
+  const secretKey = "stock-manager";
 
   
-interface userData {
-    userId: number;
-    username: string;
-}
+
+  const [username, setUsername] = useState("");
+  const [token, setToken] = useState<string>('');
+
+
+  interface userData {
+    username: string
+  }
   // Dados do usuário (ou qualquer dado que você deseja incluir no token)
   const userData = {
-    userId: 1241243124,
-    username: 'ByakkoLord',
+    username
   };
+  console.log(userData);
 
   // Gerar um token
-  function generateToken(data: userData, secret: string) {
+  function generateToken(data: string, secret: string) {
     // Converter os dados em uma string JSON
     const dataString = JSON.stringify(data);
 
     // Criar um hash HMAC usando a chave secreta
-    const token = CryptoJS.HmacSHA256(dataString, secret).toString();
-    return token;
+    const generatedToken = CryptoJS.HmacSHA256(dataString, secret).toString();
+    setToken(generatedToken);
+    return generatedToken;
+
+
   }
 
-  // Gerar um token para os dados do usuário
-  const token = generateToken(userData, secretKey);
-
-  console.log('Token gerado:', token);
-
-  return <TokenContext.Provider value={token}>{children}</TokenContext.Provider>
-}
+  return (
+    <TokenContext.Provider value={{ token ,setToken ,setUsername, generateToken, secretKey }}>
+      {children}
+    </TokenContext.Provider>
+  );
+};
