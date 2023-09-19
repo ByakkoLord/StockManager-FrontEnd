07b0a298614e0dae5958b30b9f11e585e548478a
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-19 16:01:36 -0300

    add token provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import Login from './pages/Login.tsx'
-import CreateAccount from './pages/CreateAccount.tsx'
-import Home from './pages/home/Home.tsx'
+import { TokenProvider } from "./contexts/Token.tsx";
 
+import Login from "./pages/Login.tsx";
+import CreateAccount from "./pages/CreateAccount.tsx";
+import Home from "./pages/home/Home.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-        
+    <TokenProvider>
+      <Router>
         <Routes>
-          <Route path="/" element={<Login/>} />
-          <Route path="/createAccount" element={<CreateAccount/>} />
-          <Route path='/home'element={<Home/>}/>
-          
+          <Route path="/" element={<Login />} />
+          <Route path="/createAccount" element={<CreateAccount />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
-  </React.StrictMode>,
-)
+    </TokenProvider>
+  </React.StrictMode>
+);
