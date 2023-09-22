fa2e277ace245db91762902b81cb6680d9200593
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-22 11:48:07 -0300

    new component

---
+++ src/components/Item.tsx
@@ -0,0 +1,8 @@
+export default function Item() {
+    return (
+        <article className="flex items-center justify-between border-b-2 border-slate-500 h-12 w-full">
+            <span className="ml-10 poppins text-xl text-slate-300">Table</span>
+            <span className="mr-10 poppins text-xl text-slate-300">R$500,00</span>
+        </article>
+    )
+}
