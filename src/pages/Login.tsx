726c32d6fb19744fc49d55fa0a219ad311f7012c
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-09-25 21:48:37 -0300

    add verification token in login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,13 @@
 //Api
 
+import api from '../services/api'
+
 //React Icons
 
 import { IconContext } from 'react-icons/'
 import { BsFillBoxSeamFill } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 //Components
 
@@ -12,6 +16,39 @@ import LoginBlock from './LoginBlock'
 
 
 function App() {
+
+  const navigate = useNavigate()
+    const tokenLocal = localStorage.getItem('token')
+    
+    useEffect(() => {
+        async function checkToken() {
+          if(tokenLocal){
+            try {
+                const response = await api.post('/checkToken', {
+                  token: tokenLocal
+                });
+                console.log(response.data);
+        
+                if (response.data === "Token is valid") {
+                    navigate('/home')
+                    
+                }else if (response.data === "Token is invalid"){
+                    console.log(response.data);
+                    localStorage.removeItem('token')
+                    navigate('/login')
+                }
+              } catch (error) {
+                console.error('Erro ao verificar o token:', error);
+                
+              }
+          }else{
+            navigate('/login')
+          }
+        }
+    
+        
+        checkToken();
+      }, [tokenLocal, navigate]);
 
   return (
     <div className='flex bg-gray-900 w-screen h-screen'>
